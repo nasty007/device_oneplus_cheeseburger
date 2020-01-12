1084bf75d17e9496269a2054a57a8fd68ac1af29
timocapa <timo.capa@gmail.com>
2020-01-12 22:55:47 +0100

    DU

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Open Source Project
+# Copyright (C) 2019 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,12 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosip_cheeseburger.mk
-
-COMMON_LUNCH_CHOICES := \
-    aosip_cheeseburger-user \
-    aosip_cheeseburger-userdebug \
-    aosip_cheeseburger-eng
+    $(LOCAL_DIR)/du.mk
