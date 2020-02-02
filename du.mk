132483faf0bb5f8ccd4a4319ddac2d56afa21d4e
timocapa <timo.capa@gmail.com>
2020-02-02 19:23:54 +0100

    Update copyright

--- du.mk
+++ du.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2017 The LineageOS Open Source Project
+# Copyright (C) 2017-2019 The Dirty Unicorns Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
